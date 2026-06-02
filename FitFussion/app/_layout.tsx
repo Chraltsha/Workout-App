4e0cfdb067c4660fe2bc80dabb0c5599425d9125
Alba Martin <albamartindarius@gmail.com>
2026-06-02 22:40:07 +0800

    update: added login screen and fixed index.tsx to automatically redirect to login page

--- FitFussion/app/_layout.tsx
+++ FitFussion/app/_layout.tsx
@@ -1,11 +1,5 @@
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
+  import { Stack } from 'expo-router';
 
-export default function RootLayout() {
-  return (
-    <>
-      <Stack screenOptions={{ headerShown: false }} />
-      <StatusBar style="auto" />
-    </>
-  );
-}
+  export default function RootLayout() {
+    return <Stack screenOptions={{ headerShown: false }} />;
+  }
--- FitFussion/app/index.tsx
+++ FitFussion/app/index.tsx
@@ -1,21 +1,5 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Redirect } from 'expo-router';
 
-export default function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello world</Text>
-    </View>
-  );
+export default function Index() {
+  return <Redirect href="/login" />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    flex: 1,
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-});
