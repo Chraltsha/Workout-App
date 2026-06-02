c0a640dabfdbf32c78c5326eb932a89ce86ae001
Alba Martin <albamartindarius@gmail.com>
2026-06-02 22:54:22 +0800

    addition: added signup page start and fixed login page

--- FitFussion/app/login.tsx
+++ FitFussion/app/login.tsx
@@ -1,9 +1,40 @@
-import { Text, View } from 'react-native';
+import { useRouter } from 'expo-router';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 export default function LoginScreen() {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+
+      <Pressable style={styles.button} onPress={() => router.push('/signup')}>
+        <Text style={styles.buttonText}>Signup</Text>
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 28,
+  },
+  buttonText: {
+    color: 'white',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: 'black',
+    borderRadius: 8,
+    justifyContent: 'center',
+    marginTop: 16,
+    minWidth: 120,
+    paddingHorizontal: 20,
+    paddingVertical: 12,
+  },
+});
